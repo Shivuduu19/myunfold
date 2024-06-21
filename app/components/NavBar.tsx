5b55afeda62d2fa75da4513ac5b6455e89692667
shiva kumar <98257507+Shivuduu19@users.noreply.github.com.com>
2024-06-21 12:31:12 +0530

    clear

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -34,26 +34,21 @@ const NavBar = () => {
         // Update indicator size on initial render and link clicks
         const updateIndicatorSize = () => {
             const activeLinkRef: HTMLAnchorElement | undefined = itemsRef.current.find((ref) => ref.pathname === activeLink);
-            console.log(activeLinkRef);
+            // console.log(activeLinkRef);
             setLinkClick(false)
 
 
             if (activeLinkRef) {
-                // const navParent = activeLinkRef?.parentElement.parentElement.getBoundingClientRect().left
+
                 const { width, left } = activeLinkRef.getBoundingClientRect();
                 const prevEle = activeLinkRef.previousElementSibling?.getBoundingClientRect()
-                // console.log(activeLinkRef.previousElementSibling?.getBoundingClientRect());
-                // console.log(activeLinkRef?.parentElement?.getBoundingClientRect().left);
+
+
                 let parentleft: number = 0
                 if (activeLinkRef.parentElement) {
                     parentleft = activeLinkRef.parentElement.getBoundingClientRect().left
                 }
-                // parentleft = activeLinkRef?.parentElement?.getBoundingClientRect().left;
-                // if (prevEle) {
-                //     setPrevEleWidth(prevEle.width)
-                //     setPrevEleLeft(prevEle.left - parentleft)
 
-                // }
 
                 setLeft(left - parentleft)
                 setWidth(width)
@@ -74,65 +69,19 @@ const NavBar = () => {
 
 
 
-    // }
-    // console.log(left, width);
-    // console.log(element);
-
-
-    // window.addEventListener('resize', () => {
-    //     // console.log(linkref.current);
-    //     console.log('shiva');
-    //     console.log(element);
-
-
-    //     // const resizeWidth = element.getBoundingClientRect().width
-    //     // setWidth(resizeWidth)
-    //     // console.log(resizeWidth);
-
-    // })
-    const diff = left - prevElLeft
-
-    const staicdivAnim = {
-        initial: {
-            left: left,
-            width: width
-        },
-        animate: {
-            left: left,
-            width: width
-        }
-    }
-    const divanim = {
-        initial: {
-            left: prevElLeft,
-            width: prevElWidth,
-            opacity: 1
-
-        },
-        animate: {
-            // x: diff,
-            left: left,
-            width: width,
-            opacity: 1
-        }
-    }
-
-    // function ontap(e) {
-    //     console.log(e);
-    //     e.preventDefault()
-    //     console.log('shiva');
-
-
-    // }
     function handleClick(e: any) {
         e.preventDefault()
         setLinkClick(true)
-        console.log(e.target.parentElement);
+        // console.log(e.target.parentElement);
         const parentLeft = e.target.parentElement.parentElement.getBoundingClientRect().left
-        console.log(parentLeft);
+        // console.log(parentLeft);
+        // e.target.previousElementSibling.style.opacity = 1
+        // setTimeout(() => {
+        //     e.target.parentElement.previousElementSibling.firstChild.style.opacity = 0
 
+        // }, 500)
         const { width, left } = e.target.parentElement.getBoundingClientRect()
-        console.log(left);
+        // console.log(left);
 
         setWidth(width)
         setLeft(left - parentLeft)
@@ -141,7 +90,7 @@ const NavBar = () => {
         }, 1000);
     }
 
-    console.log(width, left);
+    // console.log(width, left);
 
     return (
         <div className="z-[997] flex flex-col items-center pb-[2em] fixed  bottom-[0%] left-[0%] right-[0%] top-auto">
@@ -154,7 +103,7 @@ const NavBar = () => {
                                 arr.push(node)
                             }
                         }}>
-                        <div className="opacity-0 z-[1] bg-[#fff] rounded-[100vw] absolute top-[0%] bottom-[0%] right-[0%] left-[0%]"></div>
+                        <motion.div className="opacity-0 z-[1] bg-[#fff] rounded-[100vw] absolute top-[0%] bottom-[0%] right-[0%] left-[0%]"></motion.div>
                         <p className="z-[2] uppercase mix-blend-difference text-[1em] leading-[1] relative" >Home</p>
                     </motion.a>
                     <motion.a href="/work" onClick={(e) => handleClick(e)} className="z-[2] text-[#fff] mix-blend-difference pt-[.8em] px-[1.625em] pb-[.7em] relative max-w-full inline-block " ref={node => {
@@ -163,25 +112,29 @@ const NavBar = () => {
                             arr.push(node)
                         }
                     }}>
-                        <div className="opacity-0 z-[1] bg-[#fff] rounded-[100vw] absolute top-[0%] bottom-[0%] right-[0%] left-[0%]"></div>
+                        <motion.div className="opacity-0 z-[1] bg-[#fff] rounded-[100vw] absolute top-[0%] bottom-[0%] right-[0%] left-[0%]"></motion.div>
                         <p className="z-[2] uppercase mix-blend-difference text-[1em] leading-[1] relative">Work</p>
                     </motion.a>
+
+
                     <motion.a href="/about" onClick={(e) => handleClick(e)} className="z-[2] text-[#fff] mix-blend-difference pt-[.8em] px-[1.625em] pb-[.7em] relative max-w-full inline-block " ref={node => {
                         const arr = getRefs()
                         if (node) {
                             arr.push(node)
                         }
                     }}>
-                        <div className="opacity-0 z-[1] bg-[#fff] rounded-[100vw] absolute top-[0%] bottom-[0%] right-[0%] left-[0%]"></div>
+                        <motion.div className="opacity-0 z-[1] bg-[#fff] rounded-[100vw] absolute top-[0%] bottom-[0%] right-[0%] left-[0%]"></motion.div>
                         <p className="z-[2] uppercase mix-blend-difference text-[1em] leading-[1] relative">about</p>
                     </motion.a>
+
+
                     <motion.a href="/" onClick={(e) => handleClick(e)} className="z-[2] text-[#fff] mix-blend-difference pt-[.8em] px-[1.625em] pb-[.7em] relative max-w-full inline-block " ref={node => {
                         const arr = getRefs()
                         if (node) {
                             arr.push(node)
                         }
                     }}>
-                        <div className="opacity-0 z-[1] bg-[#fff] rounded-[100vw] absolute top-[0%] bottom-[0%] right-[0%] left-[0%]"></div>
+                        <motion.div className="opacity-0 z-[1] bg-[#fff] rounded-[100vw] absolute top-[0%] bottom-[0%] right-[0%] left-[0%]"></motion.div>
                         <p className="z-[2] uppercase mix-blend-difference text-[1em] leading-[1] relative">contact</p>
                     </motion.a>
                     <motion.div style={linkClick ? { left: left, width: width, opacity: 1, transition: 'all 500ms ease 0s', justifyContent: "flex-end" } : { left: left, width: width, opacity: 1 }} className={`z-[1] w-[6.4em]  flex justify-start absolute top-[0%] bottom-[0%] right-auto overflow-hidden opacity-0 translate-x-0`}>
